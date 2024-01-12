3d02e70eb7c34a77ae2c6a77a175c8e28eaff0ba
David-is-Doing <156245324+David-is-Doing@users.noreply.github.com>
2024-01-12 13:04:04 -0600

    I made it go beep.

--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
-
+scene.setBackgroundImage(assets.image`SMPTE`)
+music.play(music.createSoundEffect(WaveShape.Sine, 1000, 1000, 255, 255, 1000, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.LoopingInBackground)
